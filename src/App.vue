ec9ef1b30a59a5ce3354bbf73acdf43ea9ff3936
maxuecan <623875282@qq.com>
2020-08-08 01:32:26 +0800

    create

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-08 00:52:03
+ * @LastEditTime: 2020-08-08 01:07:32
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \pc-system\src\App.vue
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
---
+++ src/pages/login/login.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: your name 登录页
+ * @Date: 2020-08-08 01:14:58
+ * @LastEditTime: 2020-08-08 01:19:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \pc-system\src\pages\login\login.vue
+-->
+<template>
+
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
